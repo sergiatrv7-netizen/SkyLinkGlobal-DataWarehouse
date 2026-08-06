e09375fd1b4790ddfc55e6013d8a8d08d8537ca8
Sergio <sergiatrv7@gmail.com>
2026-08-06 14:22:14 -0600

    Update and rename 07_functions_geospatial.sql to 06_functions_geospatial.sql

---
+++ sql/06_functions_geospatial.sql
@@ -0,0 +1,5 @@
+# Geospatial Functions
+
+This folder contains the SQL scripts used to create geospatial functions and spatial calculations.
+
+This layer provides spatial analysis capabilities, including distance calculations between coordinates, route optimizations, and geographic mapping between airports and regions to enhance analytical reporting.
