d40e4158e32cf55f202c9e71b5998e0fd4d0bd15
Sergio <sergiatrv7@gmail.com>
2026-08-06 14:18:24 -0600

    Create 05_create_tables_gold.sql

---
+++ sql/05_create_tables_gold.sql
@@ -0,0 +1,5 @@
+# Gold Layer
+
+This folder contains the SQL scripts used to create the Gold layer.
+
+The Gold layer implements the dimensional model with fact and dimension tables optimized for business intelligence and reporting.
