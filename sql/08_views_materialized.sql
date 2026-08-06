cfea54a6a04fbc7705fc0cb444a3f1f161c7bee1
Sergio <sergiatrv7@gmail.com>
2026-08-06 14:23:16 -0600

    Create 08_views_materialized.sql

---
+++ sql/08_views_materialized.sql
@@ -0,0 +1,5 @@
+# Views
+
+This folder contains SQL Views and Materialized Views.
+
+These database objects simplify analytical queries, improve reporting performance, and provide reusable datasets for business users.
