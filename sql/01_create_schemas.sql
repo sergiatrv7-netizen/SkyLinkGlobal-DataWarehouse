a4e67ecb06c614ee1f2fcb1ba24899f21d188aa9
Sergio <sergiatrv7@gmail.com>
2026-08-06 14:04:19 -0600

    Create 01_create_schemas.sql

---
+++ sql/01_create_schemas.sql
@@ -0,0 +1,5 @@
+# Database Schema
+
+This folder contains the SQL scripts responsible for creating the database structure.
+
+These scripts initialize the required schemas used throughout the project, providing the foundation for the Bronze, Silver, and Gold layers of the Medallion Architecture.
