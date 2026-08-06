7764a713cd4427ff1791ceba6d41af587a9084c5
Sergio <sergiatrv7@gmail.com>
2026-08-06 14:22:54 -0600

    Create 07_triggers_audit.sql

---
+++ sql/07_triggers_audit.sql
@@ -0,0 +1,5 @@
+# Database Triggers
+
+This folder contains all database triggers implemented in the project.
+
+Triggers automate auditing, data validation, and integrity processes to ensure consistency across the Data Warehouse.
